d1d2ea28dd635c50b1d77b5e2fb966651f49f6fb
Jan <meissnej@hochschule-trier.de>
2019-03-25 16:16:28 +0100

    Added GameManager

---
+++ Assets/_Game/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public Dictionary<string, bool> UnlockedAbilities { get; private set; }
+
+	private void Awake()
+	{
+		UnlockedAbilities = new Dictionary<string, bool>
+			{{"Fire", false}, {"SuperJump", false}, {"Power", false}};
+	}
+
+	public void UnlockAbility(string ability)
+	{
+		UnlockedAbilities[ability] = true;
+	}
+
+	public void LockAbility(string ability)
+	{
+		UnlockedAbilities[ability] = false;
+	}
+}
