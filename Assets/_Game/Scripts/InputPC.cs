5ca90b07bbeb0915c4d178a5852c4b1cfd3b32a8
Frederic Nicolas Schneider <Frederic.n.Schneider@web.de>
2019-03-25 16:49:46 +0100

    Added Stone physics

--- Assets/_Game/Scripts/InputPC.cs
+++ Assets/_Game/Scripts/InputPC.cs
@@ -30,7 +30,7 @@ public class InputPC : MonoBehaviour
 		float vertical = Input.GetAxis("Vertical");
 		if(vertical != 0)
 		{
-			lemmingMovement.MoveVertical(vertical);
+			lemmingMovement.Jump(vertical);
 		}
         
     }
--- Assets/_Game/Scripts/Movement/LemmingMovement.cs
+++ Assets/_Game/Scripts/Movement/LemmingMovement.cs
@@ -7,9 +7,9 @@ public class LemmingMovement : MonoBehaviour
      [SerializeField] public float speed;
     private Rigidbody rb;
 	public bool isCrouching
-	{ get { return isCrouching; } set { isCrouching = value; } }
+	{ get; set; }
 
-	public bool inGroupe { set { inGroupe = value; } }
+	public bool inGroupe { set; get; }
 
 	private GameManager gameManager;
 
@@ -19,6 +19,7 @@ public class LemmingMovement : MonoBehaviour
     {
 		gameManager = FindObjectOfType<GameManager>();
         rb = GetComponent<Rigidbody>();
+		inGroupe = false;
     }
 
 	void FixedUpdate()
@@ -29,9 +30,19 @@ public class LemmingMovement : MonoBehaviour
 		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 	}
 
-	public void MoveVertical(float vertical)
+	public void Jump(float vertical)
 	{
-		Vector3 movement = new Vector3(0.0f, 0.0f, vertical);
+		if (!inGroupe)
+		{
+			Vector3 movement = new Vector3( 0.0f, vertical, 0.0f);
+			rb.AddForce(movement * Time.deltaTime, ForceMode.Impulse);
+		}
+
+	}
+
+	public void MoveHorizontal(float horizontal)
+	{
+		Vector3 movement = new Vector3(horizontal,0.0f, 0.0f);
 		rb.AddForce(movement * speed * Time.deltaTime);
 	}
 
