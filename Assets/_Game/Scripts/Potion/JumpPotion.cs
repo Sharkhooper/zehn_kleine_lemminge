4e91a0214b5290105de630df91fd4966cf00784e
Marcel <Marcel-e-mail@web.de>
2019-03-25 17:59:06 +0100

    Potions Korrigiert

--- Assets/_Game/Scripts/Potion/JumpPotion.cs
+++ Assets/_Game/Scripts/Potion/JumpPotion.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class JumpPotion : TileBase
+public class JumpPotion : MonoBehaviour
 {
     private GameManager inventory;
     private string name;
--- Assets/_Game/Scripts/Potion/PowerPotion.cs
+++ Assets/_Game/Scripts/Potion/PowerPotion.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class PowerPotion : TileBase
+public class PowerPotion : MonoBehaviour
 {
     private GameManager inventory;
     private string name;
