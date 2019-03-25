a25f34c67fbad1f1048de53c3afbb3e15cba27bd
KirschStreuselKuchen <m.daus@outlook.de>
2019-03-25 15:44:41 +0100

    erste Vektoren von InputTouch

--- Assets/_Game/Scripts/InputTouch.cs
+++ Assets/_Game/Scripts/InputTouch.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class InputTouch : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
-        
+		if (Input.touchCount > 0)
+		{
+			Touch touch = Input.GetTouch(0);
+			Vector3 start, movement, end;
+				start = Camera.main.ScreenToWorldPoint(touch.position);
+				Debug.Log("Touch gestartet: " + start);
+			
+			 if (touch.phase == TouchPhase.Ended)
+			{
+				end = Camera.main.ScreenToWorldPoint(touch.position);
+				Debug.Log("Touch beendet: " + end);
+			}
+			else if(touch.phase == TouchPhase.Moved)
+			{
+				movement = Camera.main.ScreenToWorldPoint(touch.position);
+				Vector3 richtungsVector = movement-start; //new Vector3(movement.x - start.x, movement.y - start.y,0)
+				Debug.Log("Touch movement: " + richtungsVector);
+			}
+
+
+		}
+
+
     }
-}
+
+
+}//class
