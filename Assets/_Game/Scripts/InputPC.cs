8e149035c3cde58d22fab2b5d7fcaa26b2cf1690
Frederic Nicolas Schneider <Frederic.n.Schneider@web.de>
2019-03-25 14:50:20 +0100

    fixed Movement
    Added pc input
    changed Input to InputTouch

---
+++ Assets/_Game/Scripts/InputPC.cs
@@ -0,0 +1,37 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputPC : MonoBehaviour
+{
+	private LemmingMovement lemmingMovement;
+
+	void Awake()
+	{
+		lemmingMovement = GetComponent<LemmingMovement>();
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		float horizontal = Input.GetAxis("Horizontal");
+		if(horizontal != 0)
+		{
+			lemmingMovement.MoveHorizontal(horizontal);
+		}
+
+		float vertical = Input.GetAxis("Vertical");
+		if(vertical != 0)
+		{
+			lemmingMovement.MoveVertical(vertical);
+		}
+        
+    }
+}
--- Assets/_Game/Scripts/Movement/LemmingMovement.cs
+++ Assets/_Game/Scripts/Movement/LemmingMovement.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class LemmingMovement : MonoBehaviour
 {
-     [Serializefield] public float speed;
+     [SerializeField] public float speed;
     private Rigidbody rb;
+	public bool isCrouching
+	{ get { return isCrouching; } set { isCrouching = value; } }
 
 
     // Start is called before the first frame update
@@ -16,11 +18,20 @@ public class Movement : MonoBehaviour
 	
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
-        rb.AddForce(movement * speed * Time.deltaTime());
+        
     }
+
+	public void MoveHorizontal(float horizontal)
+	{
+		Vector3 movement = new Vector3(horizontal, 0.0f, 0.0f);
+		rb.AddForce(movement * speed * Time.deltaTime);
+	}
+
+	public void MoveVertical(float vertical)
+	{
+		Vector3 movement = new Vector3(0.0f, 0.0f, vertical);
+		rb.AddForce(movement * speed * Time.deltaTime);
+	}
+
+
 }
