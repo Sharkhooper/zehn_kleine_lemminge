c35c921c42274b8cb2a3146e0ec5327dc05c1e7d
Jan <meissnej@hochschule-trier.de>
2019-03-26 12:09:11 +0100

    Added group controller

---
+++ Assets/_Game/Scripts/GroupController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroupController : MonoBehaviour
+{
+	public bool IsGroupSelected;
+	private LemmingMovement[] lemmings;
+	private LemmingMovement activeLemming;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+	    lemmings = transform.GetComponentsInChildren<LemmingMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void RemoveLemmingFromGroup()
+    {
+
+    }
+
+    public void KillGroup()
+    {
+	    foreach (var lemming in lemmings)
+	    {
+
+	    }
+    }
+
+    public void KillActiveLemming()
+    {
+
+    }
+}
