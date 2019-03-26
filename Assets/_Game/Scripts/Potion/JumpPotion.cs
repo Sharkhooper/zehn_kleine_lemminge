bd9f3595eaae669eac03e2678204a6e8894f4b87
Marcel <Marcel-e-mail@web.de>
2019-03-26 11:11:37 +0100

    Potions Usable (With 2D Objects)

--- Assets/_Game/Scripts/Potion/JumpPotion.cs
+++ Assets/_Game/Scripts/Potion/JumpPotion.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
 public class JumpPotion : MonoBehaviour
 {
@@ -9,24 +8,25 @@ public class JumpPotion : MonoBehaviour
     private string name;
     private void Awake()
     {
-        GameObject player = GameObject.FindWithTag("GameManager");
-        inventory =player.GetComponent<GameManager>();
+       inventory = FindObjectOfType<GameManager>(); 
     }
 
 
     void Start()
     {
 
-        string name = "SuperJump";
+        name = "SuperJump";
         
             
 
     }
 
+    
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         inventory.UnlockAbility(name);
+        Destroy(gameObject);
 
     }
 }
