328dfaf8398c728d5ff8e45c5c1c8b5c38e2b161
KirschStreuselKuchen <m.daus@outlook.de>
2019-03-27 17:40:54 +0100

    Update zum Test

--- Assets/_Game/Scripts/Button/MenuButtonScript.cs
+++ Assets/_Game/Scripts/Button/MenuButtonScript.cs
@@ -2,9 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuButtonScript : MonoBehaviour
 {
+	public int level = 1;
+	public int leben = 3;
+	public int lemminge = 10;
+
+	public Text newGameText;
+	public Text continueGameText;
+	public Text optionText;
+	public Text creditText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +29,35 @@ public class MenuButtonScript : MonoBehaviour
 
 	public void newGameButton_Click()
 	{
+		//newGameText.text = "新しい遊";
+		newGameText.color = Color.white;
 		SceneManager.LoadScene("Level 1", LoadSceneMode.Single);
+		level = 1;
+		leben = 3;
+		lemminge = 10;
+	}
+
+	public void continueGame_Click()
+	{
+		continueGameText.color = Color.white;
+		SceneManager.LoadScene("Level " + level, LoadSceneMode.Single);
+		//korrektes leben und lemming Anzahl laden
+	}
+
+	public void option_Click()
+	{
+		optionText.color = Color.white;
+		SceneManager.LoadScene("OptionScene", LoadSceneMode.Additive);
+	}
+
+	public void credit_Click()
+	{
+		creditText.color = Color.white;
+		SceneManager.LoadScene("CreditScene", LoadSceneMode.Additive);
+	}
+
+	public void back_Click()
+	{
+		SceneManager.UnloadScene(SceneManager.GetActiveScene());
 	}
 }
