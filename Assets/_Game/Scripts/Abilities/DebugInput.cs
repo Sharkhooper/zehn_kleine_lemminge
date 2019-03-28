734b03eda95cc534ef4e47a07c154ce3c03e0618
Jan <meissnej@hochschule-trier.de>
2019-03-28 15:00:30 +0100

    Added abilities, removed white background for enviroment sprites

---
+++ Assets/_Game/Scripts/Abilities/DebugInput.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugInput : MonoBehaviour
+{
+	[SerializeField] private float cooldown = 2f;
+
+	private Abilities abilities;
+	private float cooldownTimer;
+
+	private void Start()
+	{
+		abilities = GetComponent<Abilities>();
+	}
+
+	private void FixedUpdate()
+	{
+		if (cooldownTimer <= 0)
+		{
+			if (Input.GetKey(KeyCode.A))
+			{
+				abilities.Fire();
+			}
+
+			if (Input.GetKey(KeyCode.S))
+			{
+				abilities.Punch();
+			}
+
+			cooldownTimer = cooldown;
+		}
+		else
+		{
+			cooldownTimer -= Time.deltaTime;
+		}
+	}
+}
