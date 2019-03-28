01b9b687e66225eb7b0b79adaad73fbef8d37c52
Marcel <Marcel-e-mail@web.de>
2019-03-28 15:28:06 +0100

    new Interactable Switch

--- Assets/_Game/Scripts/GameObjects/InteractebaleSwitch.cs
+++ Assets/_Game/Scripts/GameObjects/InteractebaleSwitch.cs
@@ -11,8 +11,68 @@ public class InteractebaleSwitch : MonoBehaviour
     }
 
     [SerializeField] private BoolEvent onStateChange;
-    private bool statusActive=true;
+    private bool switchActiv=true;
     private bool groupIn = false;
+    private bool playerIn = false;
     
-    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+      
+        if (groupIn)
+        {
+            return;
+        }
+        
+        if (other.gameObject.CompareTag("Group"))
+        {
+            groupIn = true;
+            Debug.Log("Group Enter Trigger");
+            playerIn = true;
+
+
+        }
+        else
+        {
+            if (!other.gameObject.CompareTag(("Player"))) return;
+
+            Debug.Log("Lemming Enter Trigger");
+            playerIn = true;
+
+
+        }
+        
+        
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Group"))
+        {
+            groupIn = false;
+            playerIn = false;
+        }
+        else
+        {
+            if (!other.gameObject.CompareTag(("Player"))) return;
+
+            if (groupIn) return;
+            playerIn = false;
+
+
+        }
+    }
+
+
+    public void ActionButtonPressed()
+    {
+        if (playerIn)
+        {
+
+
+            switchActiv = !switchActiv;
+            onStateChange.Invoke(switchActiv);
+            Debug.Log(switchActiv);
+        }
+    }
 }
