2cf636d0a02553b724a54e6194ae222c13242e15
KirschStreuselKuchen <m.daus@outlook.de>
2019-03-29 16:36:22 +0100

    Ground richtig getaggt, unnötige Debug.Logs entfernt

--- Assets/_Game/Scripts/Input/InputTouch.cs
+++ Assets/_Game/Scripts/Input/InputTouch.cs
@@ -24,16 +24,12 @@ public class InputTouch : MonoBehaviour
 			touch = Input.GetTouch(0);
 			if (Input.GetTouch(0).phase == TouchPhase.Began)
 			{
-				Debug.Log("NEW TAP");
-
+				
 				start = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10));
 				originStart = start;
-				//start = touch.position;
-
-				//Debug.Log("Input START: " + start);
+				
 				if (doubleTap == 0)
 				{
-					Debug.Log("1.Touch Position X: " + start.x);
 					startZeit = Time.time;
 					touchStart = start;
 				}
@@ -45,7 +41,6 @@ public class InputTouch : MonoBehaviour
 				}
 				else
 				{
-					Debug.Log("Else-Fall");
 					tempTime = Time.time;
 					tempStart = start;
 				}
@@ -54,9 +49,7 @@ public class InputTouch : MonoBehaviour
 			else if (Input.GetTouch(0).phase == TouchPhase.Ended)
 			{
 				end = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10));
-				//end = touch.position;
-				//Debug.Log("Input END: " + end);
-
+			
 				//DoubleTap
 				{
 					if (doubleTap == 0)
@@ -66,46 +59,34 @@ public class InputTouch : MonoBehaviour
 					else if (doubleTap == 1 && Time.time - startZeit < 1f)
 					{
 						doubleTap = 0;
-						Debug.Log("DoubleTap erfolgt");
-
-						//=> Vector sollte übergeen werden, testen ob da LemmingGruppe ist
-						//RichtigeMethode
 						groupController.DoubleTab(touch);
 					}
 					else
 					{
-						Debug.Log(string.Format("Zeit: {0,6:0.0} sec.", Time.time-startZeit));
-						Debug.Log("DoubleTap zu langsam");
 						doubleTap = 0;
 						groupController.MoveHorizontal(0);
 						startZeit = tempTime;
 						touchStart = tempStart;
 					}
 				}
-
-
+				
 				richtungsVector = end - originStart;
-				//		Debug.Log("Richtung: " + richtungsVector.y);
 
-				//Jump/Croach
-
+				//Jump
 				{
 					if (richtungsVector.y > 3)
 					{
 						float tan = richtungsVector.y / richtungsVector.x;
 						if (!(tan < 1 && tan > -1 && tan != 0))
 						{
-							//Debug.Log("Tan: " + tan + "   x-Wert: " + richtungsVector.x);
 							groupController.Jump();
-							//lemming.MoveHorizontal(richtungsVector.x);
-
+							
 							//Aufstehen
 						}
 					}
 					//Vlt nicht auf nem Button möglich sein
 					else if (Mathf.Abs(richtungsVector.x) <= 2 && Mathf.Abs(richtungsVector.y) <= 2)
 					{
-						//Debug.Log("Input Punkt Richtung: " + -((Camera.main.gameObject.transform.position - end).x));
 						groupController.MoveHorizontal(end);
 					}
 				}
