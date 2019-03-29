21644dbea1192b5d6bd2fc6980d28429df69a88c
Marcel <Marcel-e-mail@web.de>
2019-03-29 13:37:52 +0100

    Made Script for BY Ability destroyable Objects and edited Layers and Prefabs of Abilitys

---
+++ Assets/_Game/Scripts/Kill/ByAbilityDestroyableObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ByAbilityDestroyableObject : MonoBehaviour,IKillTarget
+{
+    // Start is called before the first frame update
+   
+    public void Die(GameObject other)
+    {
+        Transform thisGameObject = gameObject.transform;
+
+
+
+        if (gameObject.CompareTag("Burnable") && thisGameObject.CompareTag("Fire"))
+        {
+
+
+
+
+            Destroy(thisGameObject.gameObject);
+
+        }
+        else if (gameObject.CompareTag("Explosivable")&&thisGameObject.CompareTag("Bomb"))
+        {
+            
+           
+
+            Destroy(thisGameObject.gameObject);
+        }
+
+    }
+ }
+
