42892e0ec832f57699329d3960e05f5b1d08854c
Jan <meissnej@hochschule-trier.de>
2019-03-30 17:27:01 +0100

    Bugfix: Fire not being destroyed

--- Assets/_Game/Scripts/Abilities/Fire.cs
+++ Assets/_Game/Scripts/Abilities/Fire.cs
@@ -1,11 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Fire : MonoBehaviour
+public class Fire : Deadly
 {
 	private void OnBecameInvisible()
 	{
+		Destroy(gameObject);
+	}
+
+	private void OnCollisionEnter2D(Collision2D other)
+	{
+		//Debug.Log("Collided");
+		if (isActive)
+		{
+			ExecuteEvents.Execute<IKillTarget>(other.gameObject, null, (x, y) => x.Die(gameObject));
+		}
+
 		Destroy(gameObject);
 	}
 }
