2df3feefb1614fe4f6b520542982193b6e1057a1
Marcel <Marcel-e-mail@web.de>
2019-03-30 13:42:56 +0100

    Burnable and destroyable Object done.
    Ability Inout Work in progress

--- Assets/_Game/Scripts/Kill/ByAbilityDestroyableObject.cs
+++ Assets/_Game/Scripts/Kill/ByAbilityDestroyableObject.cs
@@ -1,34 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ByAbilityDestroyableObject : MonoBehaviour,IKillTarget
 {
     // Start is called before the first frame update
    
+    [System.Serializable]
+    private class BoolEvent : UnityEvent<bool>
+    {
+    }
+
+    [SerializeField] private BoolEvent onStateChange;
+    private bool statusActive=true;
+    
+    
     public void Die(GameObject other)
     {
         Transform otherGameObject= other.transform;
-        Debug.Log("Collided");
+        //Debug.Log("Collided");
 
 
-        Debug.Log(other);
-        Debug.Log(otherGameObject);
+        //Debug.Log(other);
+      //  Debug.Log(otherGameObject);
         if (gameObject.CompareTag("Burnable") && otherGameObject.CompareTag("Fire"))
         {
 
 
 
+            
 
+            Destroy(gameObject);
+            
             Destroy(otherGameObject.gameObject);
 
         }
-        else if (gameObject.CompareTag("Explosivable")&&otherGameObject.CompareTag("Bomb"))
+        else if (gameObject.CompareTag("Explosivable")&&otherGameObject.CompareTag("Explosion"))
         {
-            
+            Debug.Log("Destroy is with bomb");
           //  Debug.Log("Collided if open");
 
-            Destroy(otherGameObject.gameObject);
+          onStateChange.Invoke(true);
+            Destroy(gameObject);
         }
 
     }
